f2ba6d624d93761523e70b34f829abc10bc7ecdd
Yash Sakre <yashsakre940@gmail.com>
2023-11-04 11:01:14 +0530

    login signup ui done

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <div className="flex pt-14 ">
+    <div className="flex flex-col pt-14 justify-center items-center">
       <div className="flex w-full h-32 justify-center items-center  ">
         <div
           className="button w-40 h-16 bg-primary  cursor-pointer select-none
@@ -14,6 +14,30 @@ function Home() {
           <span className="flex flex-col justify-center items-center h-full text-white font-bold text-lg ">
             Create Room
           </span>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center items-center mt-10">
+        <div className="text-2xl">Join Rooms</div> 
+
+        <div className="grid grid-cols-2 gap-4 mt-10 ">
+          <div className=" flex flex-col justify-between p-3 h-[20vh] w-[40vw] border-2  rounded-lg">
+            <div>01</div>
+            <div>Travel</div>
+          </div>
+          <div className=" flex flex-col justify-between p-3 h-[20vh] w-[40vw] border-2  rounded-lg">
+            <div>02</div>
+            <div>Sports</div>
+          </div>
+          <div className=" flex flex-col justify-between p-3 h-[20vh] w-[40vw] border-2  rounded-lg">
+            <div>03</div>
+            <div>Movies</div>
+          </div>
+          <div className=" flex flex-col justify-between p-3 h-[20vh] w-[40vw] border-2  rounded-lg">
+            <div>04</div>
+            <div>Cab</div>
+          </div>
+          
         </div>
       </div>
     </div>
